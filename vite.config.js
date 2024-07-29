a134444578d544adb22893780595e9dac67af3de
unknown <Eduardo Cuadra>
2024-07-29 18:34:31 -0500

    correccio vite para produccion https 2

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
         strictPort: true,
         hmr: {
             protocol: 'ws',
-            host: 'https://walrus-app-2cwac.ondigitalocean.app/', // Configura el host para HMR
+            host: 'walrus-app-2cwac.ondigitalocean.app/', // Configura el host para HMR
         },
     },
     // optimizeDeps: {
