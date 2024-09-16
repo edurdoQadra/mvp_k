da47dbc1754f9bda91bf56273b982680772cbac4
edurdoQadra <165046200+edurdoQadra@users.noreply.github.com>
2024-09-16 17:33:02 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,32 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null, // No modificar la URL base
-                    includeAbsolute: false, // No incluir URLs absolutas
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: mode !== 'production',  // Desactivar refresh en producción
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        define: {
+            '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': mode === 'production' ? 'false' : 'true',
+        },
+        optimizeDeps: {
+            exclude: ['pdfmake'],
+        },
+        build: {
+            rollupOptions: {
+                external: ['kingtech_logo_verde@4x.png'],
             },
-        }),
-    ],
-    define: {
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(true)
-    },
-    optimizeDeps: {
-        exclude: ['pdfmake']  // Excluir pdfmake de la optimización de dependencias
-    },
-    build: {
-        rollupOptions: {
-            external: ['kingtech_logo_verde@4x.png'] // Declarar la imagen como externa para que Rollup no intente resolverla
-        }
-    }
+        },
+    };
 });
