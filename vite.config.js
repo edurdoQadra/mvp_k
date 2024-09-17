0c00268a6edd7fb5f6b947d1a0d7cfcf4e0fae7e
edurdoQadra <165046200+edurdoQadra@users.noreply.github.com>
2024-09-17 18:17:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,9 +32,6 @@ export default defineConfig(({ mode }) => {
                 external: ['kingtech_logo_verde@4x.png'],  // Puedes ajustar esto si tienes otros activos externos
             },
         },
-        server: {
-        https: true,  // Habilitar HTTPS para desarrollo
-    },
-     
+
     };
 });
