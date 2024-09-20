b8b50db1ba44c16f0e428113c9dd05b888f77016
edurdoQadra <165046200+edurdoQadra@users.noreply.github.com>
2024-09-20 15:07:17 -0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+             \Fruitcake\Cors\HandleCors::class,
+    
         ],
 
         'api' => [
