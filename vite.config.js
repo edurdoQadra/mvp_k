56a24fb086d4d868ef8ff38c6b2d4ffa9e3dff64
edurdoQadra <165046200+edurdoQadra@users.noreply.github.com>
2024-09-20 15:19:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
     return {
-        base: 'https://coral-app-iuz2k.ondigitalocean.app/',
+        base: mode === 'production' ? 'https://coral-app-iuz2k.ondigitalocean.app/':'/',
         plugins: [
             laravel({
                 input: ['resources/js/app.js'],
