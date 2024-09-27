abc2cc3056b7bbcfb9b6896e2fc4f98a1a22da11
edurdoQadra <165046200+edurdoQadra@users.noreply.github.com>
2024-09-27 12:08:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-let isProduction = mode === 'production';
-
-
-    return {
-        // Deja la base como '/' para que sea relativa a cualquier dominio
-        base: isProduction ? 'http://hammerhead-app-vecpt.ondigitalocean.app/' : '/',
-        plugins: [
-            laravel({
-                input: ['resources/js/app.js'],
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
-                },
-            }),
-        ],
-        define: {
-            '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(true)
-        },
-        build: {
-            rollupOptions: {
-                external: ['/kingtech_logo_verde@4x.png'],
-            },
-            outDir: 'public/build',
-            assetsDir: 'assets',
-        },
-    };
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
